72a55bfad1a283117aa911e2b4852ed8f544da1c
mayankkasera <mayankkasera98@gmail.com>
2019-12-20 15:03:54 +0530

    Scan Done

--- app/src/main/java/com/example/qrcode/ui/fregment/QRScannerFragment.kt
+++ app/src/main/java/com/example/qrcode/ui/fregment/QRScannerFragment.kt
@@ -6,15 +6,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 
 import com.example.qrcode.R
+import kotlinx.android.synthetic.main.fragment_qrscanner.view.*
+import me.dm7.barcodescanner.zbar.Result
+import me.dm7.barcodescanner.zbar.ZBarScannerView
 
 /**
  * A simple [Fragment] subclass.
  */
-class QRScannerFragment : Fragment() {
+class QRScannerFragment : Fragment(), ZBarScannerView.ResultHandler {
 
-
+    private lateinit var mView : View
+    private lateinit var scannerView: ZBarScannerView
 
     companion object{
         fun newInstance():QRScannerFragment{
@@ -23,8 +29,85 @@ class QRScannerFragment : Fragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_qrscanner, container, false)
+        mView = inflater.inflate(R.layout.fragment_qrscanner, container, false)
+        initViews()
+        onClicks()
+        return mView.rootView
+    }
+
+    private fun initViews() {
+        initializeQRCamera()
+    }
+
+    private fun initializeQRCamera() {
+        scannerView = ZBarScannerView(context)
+        scannerView.setResultHandler(this)
+        scannerView.setBackgroundColor(ContextCompat.getColor(context!!, R.color.colorTranslucent))
+        scannerView.setBorderColor(ContextCompat.getColor(context!!, R.color.colorPrimaryDark))
+        scannerView.setLaserColor(ContextCompat.getColor(context!!, R.color.colorPrimaryDark))
+        scannerView.setBorderStrokeWidth(10)
+        scannerView.setSquareViewFinder(true)
+        scannerView.setupScanner()
+        scannerView.setAutoFocus(true)
+        startQRCamera()
+        mView.containerScanner.addView(scannerView)
+    }
+
+
+    private fun onClicks() {
+        mView.flashToggle.setOnClickListener {
+            if (mView.flashToggle.isSelected) {
+                offFlashLight()
+            } else {
+                onFlashLight()
+            }
+        }
+    }
+
+    private fun onFlashLight() {
+        mView.flashToggle.isSelected = true
+        scannerView.flash = true
+    }
+
+    private fun offFlashLight() {
+        mView.flashToggle.isSelected = false
+        scannerView.flash = false
+    }
+
+    private fun startQRCamera() {
+        scannerView.startCamera()
+    }
+
+    private fun onQrResult(contents: String?) {
+        if (contents.isNullOrEmpty())
+            showToast("Empty Qr Result")
+        else
+            showToast(contents)
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(context!!, message, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        scannerView.setResultHandler(this)
+        scannerView.startCamera()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        scannerView.stopCamera()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        scannerView.stopCamera()
+    }
+
+    override fun handleResult(rawResult: Result?) {
+        onQrResult(rawResult?.contents)
+        scannerView.resumeCameraPreview (this)
     }
 
 
